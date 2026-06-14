c126abbd3c896b9e2ca4259dab5f0a967a27fa33
maihayoon <maihayoon@edu.aac.ac.il>
2026-06-14 17:59:46 +0300

    מאיי ומאיה

--- src/main/java/org/example/LaggedMap.java
+++ src/main/java/org/example/LaggedMap.java
@@ -1,6 +1,62 @@
 package org.example;
 
-public class LaggedMap {
+import java.util.*;
+
+public class LaggedMap<K, V> {
 
 
+    private int draftSeconds;
+    private Map<K, V> officialMap = new HashMap<>();
+    private Map<K, List<Draft<V>>> draftsMap = new HashMap<>();
+    private Map<K, List<V>> historyMap = new HashMap<>();
+
+
+    public LaggedMap(int draftSeconds) {
+        this.draftSeconds = draftSeconds;
+
+        Thread backgroundThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+
+                    synchronized (LaggedMap.this) {
+                        publishReadyDrafts();
+                        cleanOldHistory();
+                    }
+
+                } catch (InterruptedException e) {
+                    System.out.println("ה-Thread ברקע נעצר.");
+                }
+            }
+        });
+
+        backgroundThread.setDaemon(true);
+        backgroundThread.start();
+    }
+
+
+    public void put(K key, V value) {
+        Draft<V> newDraft = new Draft<>(value, false, draftSeconds);
+
+        if (!draftsMap.containsKey(key)) {
+            draftsMap.put(key, new ArrayList<>());
+        }
+
+        draftsMap.get(key).add(newDraft);
+
+        System.out.println("השינוי נכנס לטיוטה וממתין לפרסום!");
+    }
+
+
+    private void publishReadyDrafts() {
+    }
+
+    private void cleanOldHistory() {
+        for (List<V> historyList : historyMap.values()) {
+
+            while (historyList.size() > 3) {
+                historyList.remove(0);
+            }
+        }
+    }
 }
